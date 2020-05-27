ebd2fec89379a8ea2e2362af2087a5a3b897ba29
Ilia Mitvichin <mitvichin@gmail.com>
2020-05-27 19:16:33 +0300

    added modal closing when backdrop is clicked

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -7,7 +7,7 @@ import { delay } from '../../utils/delay';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
-  @Input() title:string = "You have forgot to pass a tittle!  "
+  @Input() title: string = "You have forgot to pass a tittle!  "
 
   @ViewChild('content') content: ElementRef;
 
@@ -20,36 +20,41 @@ export class ModalComponent implements OnInit {
   async open() {
     this.renderer.setStyle(this.hostRef.nativeElement, 'display', 'block')
     await delay(50)
-    
+
     this.renderer.setStyle(this.hostRef.nativeElement, 'opacity', '1')
     this.renderer.setStyle(this.content.nativeElement, 'opacity', '1')
-    
-    this.renderer.setStyle(this.hostRef.nativeElement, 'width', '100%')
-    this.renderer.setStyle(this.content.nativeElement,'width', '50%' )
-    
-    this.renderer.setStyle(this.hostRef.nativeElement, 'height', '100%')
-    this.renderer.setStyle(this.content.nativeElement,'height', '50%' )
-    
-    this.renderer.setStyle(this.hostRef.nativeElement, 'border-radius', '0')
-    this.renderer.setStyle(this.content.nativeElement,'border-radius', '.2rem' )
 
-   
+    this.renderer.setStyle(this.hostRef.nativeElement, 'width', '100%')
+    this.renderer.setStyle(this.content.nativeElement, 'width', '50%')
+
+    this.renderer.setStyle(this.hostRef.nativeElement, 'height', '100%')
+    this.renderer.setStyle(this.content.nativeElement, 'height', '50%')
+
+    this.renderer.setStyle(this.hostRef.nativeElement, 'border-radius', '0')
+    this.renderer.setStyle(this.content.nativeElement, 'border-radius', '.2rem')
+
+
+  }
+
+  stopPropagation(e: Event) {
+    e.stopImmediatePropagation();
+    e.stopPropagation();
   }
 
   async close() {
     this.renderer.setStyle(this.hostRef.nativeElement, 'opacity', '0')
     this.renderer.setStyle(this.content.nativeElement, 'opacity', '0')
     this.renderer.setStyle(this.hostRef.nativeElement, 'border-radius', '50%')
-    this.renderer.setStyle(this.content.nativeElement,'border-radius', '50%' )
+    this.renderer.setStyle(this.content.nativeElement, 'border-radius', '50%')
     this.renderer.setStyle(this.hostRef.nativeElement, 'width', '0')
-    this.renderer.setStyle(this.content.nativeElement,'width', '0' )
+    this.renderer.setStyle(this.content.nativeElement, 'width', '0')
     this.renderer.setStyle(this.hostRef.nativeElement, 'height', '0')
-    this.renderer.setStyle(this.content.nativeElement,'height', '0' )
+    this.renderer.setStyle(this.content.nativeElement, 'height', '0')
 
     await delay(400)
     this.renderer.setStyle(this.hostRef.nativeElement, 'display', 'none')
 
-    
+
   }
 
 }
